24da55d4e103c1e14c001e1718a26661ba356c49
jentachibana <jentachibana@gmail.com>
2023-01-27 14:50:39 -0800

    kitty kitty

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import TodoList from './TodoList'
 
 function App() {
-  const [thingstodo, setTodos] = useState(['Wash dishes', 'Clean bathroom', "Brush teeth"])
+  const [currentListState, setTodos] = useState(['Wash dishes', 'Clean bathroom', "Brush teeth"])
   return (
     <>
       <h2> TO DO LIST </h2>
-      <TodoList todos={thingstodo} />
+      kitty
+      kitty
+       <TodoList todos={currentListState} />
       <input type='text' />
       <button> add item </button>
       <button> clear </button>
