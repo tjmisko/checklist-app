bed7664ce48fd490304b09c3324b1ed197af0ceb
jentachibana <jentachibana@gmail.com>
2023-01-27 12:16:21 -0800

    added buttons and stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import TodoList from './TodoList'
 
 function App() {
   return (
+    <>
+      <h2> TO DO LIST </h2>
+      
       <TodoList />
+
+      <input type='text' />
+      <button> add item </button>
+      <button> clear </button>
+    </>
   )
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function TodoList() {
     return (
         <div>
-            Hello World
+            CHECKLIST
         </div>
     )
 }
